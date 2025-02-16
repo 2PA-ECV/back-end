9104cbb07bf66d1d5ed9a908c5dc6b6398038225
xavialgarraa <131766832+xavialgarraa@users.noreply.github.com>
2025-02-16 23:53:14 +0100

    Update userModel.js to adapt it to the DB

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,19 @@
 const db = require('../config/database');
 
 const User = {
-  create: (name, email, password, birth_date, gender, city, profile_picture, callback) => {
-    db.query(
-      'INSERT INTO users (name, email, password, birth_date, gender, city, profile_picture) VALUES (?, ?, ?, ?, ?, ?, ?)',
-      [name, email, password, birth_date, gender, city, profile_picture],
-      callback
-    );
-  },
-
-  findByEmail: (email, callback) => {
-    db.query('SELECT * FROM users WHERE email = ?', [email], callback);
-  }
-};
-
+    create: (userData, callback) => {
+      const { name, email, password, birth_date, gender, city, profile_picture } = userData;
+  
+      db.query(
+        'INSERT INTO users (name, email, password, birth_date, gender, city, profile_picture) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        [name, email, password, birth_date, gender, city, profile_picture],
+        callback
+      );
+    },
+  
+    findByEmail: (email, callback) => {
+      db.query('SELECT * FROM users WHERE email = ?', [email], callback);
+    }
+  };
+  
 module.exports = User;
